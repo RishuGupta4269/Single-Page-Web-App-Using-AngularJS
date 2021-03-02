1b7ee6151eac111d5ea37796d84078e3667ae26d
Rishu Gupta <rishugupta.4269@gmail.com>
2021-03-02 14:49:45 +0530

    Module_1 done

---
+++ assignment1-solution/app.js
@@ -0,0 +1,48 @@
+(function(){
+'use strict';
+
+
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchChecker);
+
+LunchChecker.$inject=['$scope'];
+
+function LunchChecker($scope){
+	$scope.in="";
+	$scope.message="";
+	$scope.empty=false;
+	$scope.check_start=false;
+	
+	$scope.dispMSG=function(){
+		var s=$scope.in;
+		if(s.trim().length==0)
+			$scope.empty=true;
+		else{
+			$scope.check_start=true;
+			$scope.empty=false;
+			var arr=s.split(',');
+			var msg="";
+			var k=0;
+			for(var i=0;i<arr.length;i++){
+				if(arr[i].trim().length>0){
+					k++;
+				}
+			}
+			if(k==0){
+				$scope.empty=true;
+			}
+			else
+			if(k<=3){
+				msg="Enjoy!";
+			}
+			else
+				msg="Too much!";
+
+			$scope.message=msg;
+		}
+	}
+}
+
+
+
+})();
